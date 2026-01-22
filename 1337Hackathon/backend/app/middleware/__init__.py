a13a06307b117544c854e13c2b11c4302af0d84a
Zero <ZERO_BROTHER12345@outlook.com>
2026-01-22 22:08:39 +0100

    rm

--- 1337Hackathon/backend/app/middleware/__init__.py
+++
@@ -1,2 +0,0 @@
-# Middleware
-from app.middleware.auth import get_current_user, get_current_user_optional, get_staff_user
--- 1337Hackathon/backend/app/services/__init__.py
+++
@@ -1,3 +0,0 @@
-# Services
-from app.services.jwt_service import create_access_token, verify_token
-from app.services.ft_api import ft_api
